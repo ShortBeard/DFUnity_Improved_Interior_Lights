4a7b843dcebda7ab544e3046111b1904ac370b6f
ShortBeard <shaneh2505@gmail.com>
2020-10-06 19:04:44 +1000

    v1.0.3

--- BillboardShadows.cs
+++ BillboardShadows.cs
@@ -1,33 +1,40 @@
 ///////////////////////////////////////////////////////////
 /// Mod: Improved Interior Lighting
 /// Author: ShortBeard
-/// Version: 1.0.2
+/// Version: 1.0.3
 /// Description: Creates warmer interior & dungeon lights.
 ///////////////////////////////////////////////////////////
-///
+
 using DaggerfallWorkshop;
 using DaggerfallWorkshop.Game;
 using System.Collections;
 using UnityEngine;
 
-namespace ImprovedInteriorLighting {
+namespace ImprovedInteriorLighting
+{
 
     /// <summary>
     /// This class just contains some static methods to put two-sided shadows on all NPC's or enemies
     /// </summary>
-    public class BillboardShadows : MonoBehaviour {
+    public class BillboardShadows : MonoBehaviour
+    {
+
 
         /// <summary>
         /// Toggles shadow casting on all static NPC's in any given interior
         /// </summary>
         /// <param name="args"></param>
-        public static void ToggleIndoorNPCBillboardShadows(bool shadowsEnabled) {
+        public static void ToggleIndoorNPCBillboardShadows(bool shadowsEnabled)
+        {
             StaticNPC[] npcBillboards = (StaticNPC[])FindObjectsOfType(typeof(StaticNPC)); //Get all static NPC's in the scene
-            foreach (StaticNPC npc in npcBillboards) {
-                if (shadowsEnabled == true) {
+            foreach (StaticNPC npc in npcBillboards)
+            {
+                if (shadowsEnabled == true)
+                {
                     npc.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided; //Give their mesh renderer a shadow
                 }
-                else {
+                else
+                {
                     npc.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
                 }
             }
@@ -38,7 +45,8 @@ namespace ImprovedInteriorLighting {
         /// so that we have access to StartCoroutine()
         /// </summary>
         /// <param name="args"></param>
-        public static void ToggleDungeonEnemyBillboardShadows(bool shadowsEnabled, MonoBehaviour monoBehaviour) {
+        public static void ToggleDungeonEnemyBillboardShadows(bool shadowsEnabled, MonoBehaviour monoBehaviour)
+        {
             monoBehaviour.StartCoroutine(ApplyShadows(shadowsEnabled));
         }
 
@@ -50,18 +58,28 @@ namespace ImprovedInteriorLighting {
         /// </summary>
         /// <param name="shadowsEnabled"></param>
         /// <returns></returns>
-        private static IEnumerator ApplyShadows(bool shadowsEnabled) {
+        private static IEnumerator ApplyShadows(bool shadowsEnabled)
+        {
+           
             yield return new WaitForSeconds(1);
             DaggerfallEnemy[] enemyBillboards = (DaggerfallEnemy[])FindObjectsOfType(typeof(DaggerfallEnemy));
-            foreach (DaggerfallEnemy enemy in enemyBillboards) {
-                if (shadowsEnabled == true) {
-                    enemy.transform.GetChild(0).GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided; //Give their mesh renderer a shadow
-                }
-                else {
-                    enemy.transform.GetChild(0).GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            foreach (DaggerfallEnemy enemy in enemyBillboards)
+            {
+                MeshRenderer enemyRenderer = enemy.GetComponentInChildren(typeof(MeshRenderer)) as MeshRenderer; //Get mesh renderer component in enemy child
+
+                if (enemyRenderer != null)
+                {
+                    //Add shadows if the setting is enabled, and the enemy is marked by the engine to cast a shadow OR the option is toggled to force shadows on ALL enemies regardless of what the engine marks it as.
+                    if ((shadowsEnabled == true && enemy.gameObject.GetComponentInChildren<DaggerfallMobileUnit>().Summary.Enemy.NoShadow == false) || (shadowsEnabled == true && ImproveDungeonLighting.dungeonModSettings.EnemyShadowsNoShadowEnemies == true))
+                    {
+                        enemyRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided; //Give their mesh renderer a shadow. This will take a second to apply as per the above coroutine that waits 1 second.
+                    }
+                    else
+                    {
+                        enemyRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off; //Disable mesh renderer shadow
+                    }
                 }
             }
-
         }
 
     }
